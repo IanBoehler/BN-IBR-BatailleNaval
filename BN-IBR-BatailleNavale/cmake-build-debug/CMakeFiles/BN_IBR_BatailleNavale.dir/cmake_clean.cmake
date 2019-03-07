ee0418033e4a93bc0903b5c1ef3106bb0375121c
IanBoehler <47849852+IanBoehler@users.noreply.github.com>
2019-03-07 15:46:02 +0100

    Hello World

---
+++ BN-IBR-BatailleNavale/cmake-build-debug/CMakeFiles/BN_IBR_BatailleNavale.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BN_IBR_BatailleNavale.dir/main.c.obj"
+  "BN_IBR_BatailleNavale.pdb"
+  "BN_IBR_BatailleNavale.exe"
+  "BN_IBR_BatailleNavale.exe.manifest"
+  "libBN_IBR_BatailleNavale.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BN_IBR_BatailleNavale.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ BN-IBR-BatailleNavale/cmake-build-debug/CMakeFiles/BN_IBR_BatailleNavale.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BN_IBR_BatailleNavale.
+# This may be replaced when dependencies are built.
